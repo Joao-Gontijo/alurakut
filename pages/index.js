91da8ec380450945096a5d07e8b442970a779fc7
Joao-Gontijo <jvgontijo166@gmail.com>
2021-07-15 23:48:33 -0300

    Alurakut-aula-03

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,28 @@ function ProfileSidebar(propriedades) {
     )
 }
 
+function ProfileRelationsBox(propriedades){
+    return(
+        <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+                {propriedades.title} ({propriedades.items.length})
+            </h2>
+            {/* <ul>
+                {comunidades.map((itemAtual) => {
+                    return (
+                        <li key={itemAtual.id}>
+                            <a href={`/users/${itemAtual.title}`}>
+                                <img src={itemAtual.image} />
+                                <span>{itemAtual.title}</span>
+                            </a>
+                        </li>
+                    )
+                })}
+            </ul> */}
+        </ProfileRelationsBoxWrapper>
+    )
+}
+
 export default function Home() {
     const user = 'Joao-Gontijo';
     const [comunidades, setComunidades] = React.useState([{
@@ -33,8 +55,18 @@ export default function Home() {
         'gabrielifg',
         'aldo-pereira22',
         'SamuelGomesRocha',
-        'EuripedesVieira']
+        'EuripedesVieira'];
 
+    const [seguidores, setSeguidores] = React.useState([]);
+
+    React.useEffect(function(){
+        const seguidores = fetch('https://api.github.com/users/Joao-Gontijo/followers').then(function (respostaDoServidor){
+            return respostaDoServidor.json();
+        }).then(function(respostaCompleta){
+            setSeguidores(respostaCompleta);
+        })
+    }, [])
+    
     return (
         <>
             <AlurakutMenu githubUser={user}/>
@@ -88,6 +120,7 @@ export default function Home() {
                 </div>
 
                 <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+                    <ProfileRelationsBox title="Seguidores" items={seguidores}/>
                     <ProfileRelationsBoxWrapper>
                         <h2 className="smallTitle">
                             Comunidades ({comunidades.length})
@@ -113,7 +146,7 @@ export default function Home() {
                             {pessoasFavoritas.map((itemAtual) => {
                                 return (
                                     <li key={itemAtual}>
-                                        <a href={`/users/${itemAtual}`}>
+                                        <a href={`https://github.com/${itemAtual}`}>
                                             <img src={`https://github.com/${itemAtual}.png`} />
                                             <span>{itemAtual}</span>
                                         </a>
